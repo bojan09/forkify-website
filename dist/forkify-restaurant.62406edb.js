99cea7cabd8616c975cd94a8bb8e6df903eabe43
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2025-09-04 07:13:52 +0200

    fetch Api

--- dist/forkify-restaurant.62406edb.js
+++ dist/forkify-restaurant.62406edb.js
@@ -6,6 +6,28 @@ const timeout = function(s) {
         }, s * 1000);
     });
 };
-console.log('test');
+const showRecipe = async ()=>{
+    try {
+        const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8433');
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        console.log(data, res);
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingridients: recipe.ingridients
+        };
+        console.log(recipe);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
 
 //# sourceMappingURL=forkify-restaurant.62406edb.js.map
--- src/js/controller.js
+++ src/js/controller.js
@@ -8,4 +8,33 @@ const timeout = function (s) {
   });
 };
 
-console.log('test');
+const showRecipe = async () => {
+  try {
+    const res = await fetch(
+      'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8433'
+      // 'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    console.log(data, res);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingridients: recipe.ingridients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
